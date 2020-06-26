ded3fb05195e123fa77a867d923b139d43a96b65
Aleksey Ostrikov <alex303606@gmail.com>
2020-06-26 17:47:10 +0200

    fixed font scaling

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import { Provider } from 'react-redux';
 import React from 'react';
 import { persistor, store } from './store/configureStore';
 import { PersistGate } from 'redux-persist/lib/integration/react';
-import { Dimensions } from 'react-native';
+import { Dimensions, Text } from 'react-native';
 import AppNavigationState from './navigation/AppNavigationState';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import Loader from './components/Loader';
 
 const entireScreenWidth = Dimensions.get('window').width;
 EStyleSheet.build({$rem: entireScreenWidth / 414});
+
+if (Text && !Text.defaultProps) {
+	Text.defaultProps = {};
+	Text.defaultProps.allowFontScaling = false;
+}
 
 export default class App extends React.Component {
 	render() {
