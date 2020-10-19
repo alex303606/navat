a88b8f63a1afd44684ec5cfef2765b9fd82edf6c
Aleksey Ostrikov <aleksey.ostrikov@instamart.ru>
2020-10-19 18:52:39 +0200

    BranchesScreen.jsx

--- src/screens/BranchesScreen.jsx
+++ src/screens/BranchesScreen.jsx
@@ -4,18 +4,30 @@ import {connect} from 'react-redux';
 import config from '../../config';
 import {SliderBox} from "react-native-image-slider-box";
 import EStyleSheet from "react-native-extended-stylesheet";
-import {Text} from '../components/Texts';
+import {Text, Bold, MiddleText} from '../components/Texts';
 
 const styles = EStyleSheet.create({
     page: {
         flex: 1,
         backgroundColor: config.BackgroundColor
     },
+    content: {
+        padding: '20rem',
+    },
+    bold: {
+        marginBottom: '5rem',
+        marginTop: '30rem',
+    },
+    middle: {
+        marginBottom: '11rem',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
 });
 
 const BranchesScreen = props => {
     const branch = props.navigation.getParam('branch');
-    console.log(branch);
+
     return (
         <View style={styles.page}>
             <SliderBox
@@ -30,10 +42,14 @@ const BranchesScreen = props => {
                 ImageLoader={() => <View/>}
                 imageLoadingColor={'transparent'}
             />
-            <Text>{branch.name}</Text>
-            <Text>{branch.address}</Text>
-            <Text>{branch.time}</Text>
-            <Text>{branch.about}</Text>
+            <View style={styles.content}>
+                <Bold style={styles.bold}>{branch.name}</Bold>
+                <View style={styles.middle}>
+                    <MiddleText>{branch.address}</MiddleText>
+                    <MiddleText>{branch.time}</MiddleText>
+                </View>
+                <Text>{branch.about}</Text>
+            </View>
         </View>
     );
 };
