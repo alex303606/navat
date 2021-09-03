bad866e39bf7246609aee3446e6ead19ba7fd85b
Aleksey Ostrikov <aleksey.ostrikov@instamart.ru>
2021-09-03 14:48:23 +0200

    fix

--- config.js
+++ config.js
@@ -1,87 +1,87 @@
 export default {
-	shadowOptOpacity: 0.15,
-	TabBarHeight: 64,
-	MainColor: '#1E8149',
-	LoaderColor: '#C1C0C9',
-	InactiveColorFunc: (opacity) => `rgba(30, 129, 73, ${opacity})`,
-	BackgroundColor: '#F5F5F5',
-	GreyColor: '#C1C0C9',
+  shadowOptOpacity: 0.15,
+  TabBarHeight: 64,
+  MainColor: '#1E8149',
+  LoaderColor: '#C1C0C9',
+  InactiveColorFunc: opacity => `rgba(30, 129, 73, ${opacity})`,
+  BackgroundColor: '#F5F5F5',
+  GreyColor: '#C1C0C9',
 };
 
 export const countries = {
-	Bishkek: {
-		flag: require('./src/assets/images/kg.jpg'),
-		code: '996',
-		phoneMask: '0 999 99-99-99',
-		city: 'Бишкек',
-		apiUrl: 'http://navat.kg/dostavka/app',
-		imageUrl: 'http://navat.kg/dostavka/files/products/',
-		deliveryThreshold: 250,
-		deliveryPrice: 200,
-		deliveryAfterSalePrice: 0,
-		phones: {
-			deliveryService: {
-				call: '+996551641111',
-				display: '+996 551 64-11-11'
-			},
-			commentsSuggestions: {
-				call: '+996551631111',
-				display: '+996 551 63-11-11'
-			},
-		},
-		branches: [
-			{
-				name: 'Чайхана Navat на Сейфуллина',
-				address: 'Казахстан, Алматы, проспект Сейфуллина 500/79',
-				time: '10:00 - 24:00',
-				images: [
-					'http://navat.com.kz/app/rest/img01.jpg',
-					'http://navat.com.kz/app/rest/img02.jpg',
-					'http://navat.com.kz/app/rest/img03.jpg',
-					'http://navat.com.kz/app/rest/img04.jpg',
-					'http://navat.com.kz/app/rest/img05.jpg'
-				],
-				rating: 5
-			},
-			{
-				name: 'Чайхана Navat на пр. Достык',
-				address: 'Казахстан, Алматы, проспект Достык 48',
-				time: '10:00 - 24:00',
-				images: [
-					'http://navat.com.kz/app/rest/img11.jpg',
-					'http://navat.com.kz/app/rest/img12.jpg',
-					'http://navat.com.kz/app/rest/img13.jpg',
-					'http://navat.com.kz/app/rest/img14.jpg',
-					'http://navat.com.kz/app/rest/img15.jpg'
-				],
-				rating: 5
-			},
-			{
-				name: 'Чайхана Navat на Аль-Фараби',
-				address: 'Казахстан, Алматы, проспект Аль-Фараби, 77/1 ',
-				time: '10:00 - 24:00',
-				images: [
-					'http://navat.com.kz/app/rest/img21.jpg',
-					'http://navat.com.kz/app/rest/img22.jpg',
-					'http://navat.com.kz/app/rest/img23.jpg',
-					'http://navat.com.kz/app/rest/img24.jpg',
-					'http://navat.com.kz/app/rest/img25.jpg'
-				],
-				rating: 5
-			},
-			{
-				name: 'Чайхана Navat на Абылайхана',
-				address: 'Казахстан, Алматы, проспект Абылайхана, 58 А ',
-				time: '10:00 - 24:00',
-				images: [
-					'http://navat.com.kz/app/rest/img31.jpg',
-					'http://navat.com.kz/app/rest/img32.jpg',
-					'http://navat.com.kz/app/rest/img33.jpg',
-					'http://navat.com.kz/app/rest/img34.jpg',
-					'http://navat.com.kz/app/rest/img35.jpg'
-				],
-				rating: 5
-			},
-		],
-	},
+  Bishkek: {
+    flag: require('./src/assets/images/kg.jpg'),
+    code: '996',
+    phoneMask: '0 999 99-99-99',
+    city: 'Бишкек',
+    apiUrl: 'http://navat.kg/dostavka/app',
+    imageUrl: 'http://navat.kg/dostavka/files/products/',
+    deliveryThreshold: 250,
+    deliveryPrice: 200,
+    deliveryAfterSalePrice: 0,
+    phones: {
+      deliveryService: {
+        call: '+996551641111',
+        display: '+996 551 64-11-11',
+      },
+      commentsSuggestions: {
+        call: '+996551631111',
+        display: '+996 551 63-11-11',
+      },
+    },
+    branches: [
+      {
+        name: 'Чайхана Navat на Сейфуллина',
+        address: 'Казахстан, Алматы, проспект Сейфуллина 500/79',
+        time: '10:00 - 24:00',
+        images: [
+          'http://navat.com.kz/app/rest/img01.jpg',
+          'http://navat.com.kz/app/rest/img02.jpg',
+          'http://navat.com.kz/app/rest/img03.jpg',
+          'http://navat.com.kz/app/rest/img04.jpg',
+          'http://navat.com.kz/app/rest/img05.jpg',
+        ],
+        rating: 5,
+      },
+      {
+        name: 'Чайхана Navat на пр. Достык',
+        address: 'Казахстан, Алматы, проспект Достык 48',
+        time: '10:00 - 24:00',
+        images: [
+          'http://navat.com.kz/app/rest/img11.jpg',
+          'http://navat.com.kz/app/rest/img12.jpg',
+          'http://navat.com.kz/app/rest/img13.jpg',
+          'http://navat.com.kz/app/rest/img14.jpg',
+          'http://navat.com.kz/app/rest/img15.jpg',
+        ],
+        rating: 5,
+      },
+      {
+        name: 'Чайхана Navat на Аль-Фараби',
+        address: 'Казахстан, Алматы, проспект Аль-Фараби, 77/1 ',
+        time: '10:00 - 24:00',
+        images: [
+          'http://navat.com.kz/app/rest/img21.jpg',
+          'http://navat.com.kz/app/rest/img22.jpg',
+          'http://navat.com.kz/app/rest/img23.jpg',
+          'http://navat.com.kz/app/rest/img24.jpg',
+          'http://navat.com.kz/app/rest/img25.jpg',
+        ],
+        rating: 5,
+      },
+      {
+        name: 'Чайхана Navat на Абылайхана',
+        address: 'Казахстан, Алматы, проспект Абылайхана, 58 А ',
+        time: '10:00 - 24:00',
+        images: [
+          'http://navat.com.kz/app/rest/img31.jpg',
+          'http://navat.com.kz/app/rest/img32.jpg',
+          'http://navat.com.kz/app/rest/img33.jpg',
+          'http://navat.com.kz/app/rest/img34.jpg',
+          'http://navat.com.kz/app/rest/img35.jpg',
+        ],
+        rating: 5,
+      },
+    ],
+  },
 };
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import React from 'react';
-import { persistor, store } from './store/configureStore';
-import { PersistGate } from 'redux-persist/lib/integration/react';
-import { Dimensions, Text } from 'react-native';
+import {persistor, store} from './store/configureStore';
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import {Dimensions, Text} from 'react-native';
 import AppNavigationState from './navigation/AppNavigationState';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import Loader from './components/Loader';
@@ -11,18 +11,18 @@ const entireScreenWidth = Dimensions.get('window').width;
 EStyleSheet.build({$rem: entireScreenWidth / 414});
 
 if (Text && !Text.defaultProps) {
-	Text.defaultProps = {};
-	Text.defaultProps.allowFontScaling = false;
+  Text.defaultProps = {};
+  Text.defaultProps.allowFontScaling = false;
 }
 
 export default class App extends React.Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<PersistGate loading={<Loader/>} persistor={persistor}>
-					<AppNavigationState/>
-				</PersistGate>
-			</Provider>
-		);
-	}
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate loading={<Loader />} persistor={persistor}>
+          <AppNavigationState />
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
